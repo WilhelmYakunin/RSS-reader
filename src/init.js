52d86b4bbb38d7a116892751141e5be2696edf9f
WilhelmYakunin <50325829+WilhelmYakunin@users.noreply.github.com>
2021-03-09 22:48:49 +0300

    Update init.js

--- src/init.js
+++ src/init.js
@@ -1,7 +1,6 @@
 import i18next from 'i18next';
 import * as yup from 'yup';
 import onChange from 'on-change';
-import axios from 'axios';
 import _ from 'lodash';
 import fetch from 'node-fetch';
 import { i18n, languages } from './locales/i18nEngine';
